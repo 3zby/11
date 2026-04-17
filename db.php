4c7869a00603c1d1e734b977549e2bb1ed2bd183
DFkz <146791648+3zby@users.noreply.github.com>
2026-04-17 12:20:57 +0300

    Update db.php

--- db.php
+++ db.php
@@ -1,9 +1,8 @@
 <?php
+
 $pdo = new PDO(
     "pgsql:host=dpg-d7gurdfavr4c73ai92h0-a.oregon-postgres.render.com;port=5432;dbname=for_api",
     "for_api_user",
-    "kOq9uc9bdn0CTgIZoOFIE1BJ9wJyXTnU"
+    "YOUR_PASSWORD",
+    [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]
 );
-
-$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-?>
