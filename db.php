9c50e3773aad9e4009cb741f0c852ae96d20ad1f
DFkz <146791648+3zby@users.noreply.github.com>
2026-04-17 12:36:38 +0300

    Update db.php

--- db.php
+++ db.php
@@ -5,8 +5,17 @@ $db   = getenv("DB_NAME");
 $user = getenv("DB_USER");
 $pass = getenv("DB_PASS");
 
+if (!$host || !$db || !$user || !$pass) {
+    die("Missing DB env variables");
+}
+
 $dsn = "pgsql:host=$host;port=5432;dbname=$db;sslmode=require";
 
-$pdo = new PDO($dsn, $user, $pass, [
-    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-]);
+try {
+    $pdo = new PDO($dsn, $user, $pass, [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_TIMEOUT => 10
+    ]);
+} catch (PDOException $e) {
+    die("DB ERROR: " . $e->getMessage());
+}
